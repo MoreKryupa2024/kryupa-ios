dbfb146929f720f2a89324f3d614f11f576d6a0e
Pooja Nenava <poojanenava1801@gmail.com>
2024-06-05 13:33:03 +0530

    HomePageCareGiver

---
+++ Kryupa/Components/CareGiverHome/CareGiverPortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  CareGiverPortfolioView.swift
+//  Kryupa
+//
+//  Created by Pooja Nenava on 04/06/24.
+//
+
+import SwiftUI
+
+struct CareGiverPortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CareGiverPortfolioView()
+}
