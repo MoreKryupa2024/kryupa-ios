253e4e3bfde2fdf905dea6476833a22f23135162
poojaNenavaMoreYeahs <pooja.nenava@moreyeahs.in>
2024-06-06 17:56:33 +0530

    Chat UI Care giver

--- Kryupa/Components/Chat/ChatBoxView.swift
+++ Kryupa/Components/Chat/ChatBoxView.swift
@@ -8,11 +8,142 @@
 import SwiftUI
 
 struct ChatBoxView: View {
+    
+    var msg: Message
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        switch msg.chatboxType {
+        case .currentUser:
+            HStack {
+                Spacer()
+                
+                Text("\(msg.content)")
+                    .frame(alignment: .trailing)
+                    .font(.custom(FontContent.plusRegular, size: 13))
+                    .foregroundColor(Color.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background {
+                        Image("MessageBubbleCurrentUser")
+                            .resizable()
+                    }
+            }
+            .padding(.horizontal, 20)
+            
+        case .otherUser:
+            HStack {
+                Text("\(msg.content)")
+                    .font(.custom(FontContent.plusRegular, size: 13))
+                    .foregroundStyle(._242426)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    .background {
+                        Image("MessageBubbleOtherUser")
+                            .resizable()
+                    }
+                
+                Spacer()
+
+            }
+            .padding(.horizontal, 20)
+            
+        case .currentUserViewService:
+            
+            HStack {
+                Spacer()
+                
+            VStack(alignment: .leading) {
+                Text("\(msg.content)")
+                    .font(.custom(FontContent.plusRegular, size: 13))
+                    .foregroundColor(Color.white)
+                    .padding(.horizontal, 20)
+
+                Text("View Service")
+                    .font(.custom(FontContent.plusRegular, size: 16))
+                    .foregroundStyle(.white)
+                    .frame(width: 138, height: 32)
+                    .background{
+                        RoundedRectangle(cornerRadius: 48)
+                    }
+                    .asButton(.press) {
+                        
+                    }
+                    .padding(.horizontal, 20)
+            }
+            .padding(.vertical, 15)
+            .background {
+                Image("MessageBubbleCurrentUser")
+                    .resizable()
+            }
+            }
+            .padding(.horizontal, 20)
+            
+        case .otherUserViewService:
+            HStack {
+            VStack(alignment: .leading) {
+                Text("\(msg.content)")
+                    .font(.custom(FontContent.plusRegular, size: 13))
+                    .foregroundStyle(._242426)
+                    .padding(.horizontal, 20)
+                
+                Text("View Service")
+                    .font(.custom(FontContent.plusRegular, size: 16))
+                    .foregroundStyle(.white)
+                    .frame(width: 138, height: 32)
+                    .background{
+                        RoundedRectangle(cornerRadius: 48)
+                    }
+                    .asButton(.press) {
+                        
+                    }
+                    .padding(.horizontal, 20)
+
+                    
+            }
+            .padding(.vertical, 15)
+            .background {
+                Image("MessageBubbleOtherUser")
+                    .resizable()
+            }
+                Spacer()
+
+        }
+        .padding(.horizontal, 20)
+        }
     }
 }
 
+struct Message: Hashable {
+    var id = UUID()
+    var content: String
+    var chatboxType: ChatBoxType
+}
+
+struct DataSource {
+    
+    static let messages = [
+        
+        Message(content: "Hello [User's Name],\nI am interested in your profile.", chatboxType: .currentUser),
+        
+        Message(content: "Hello! How can I assist you today?", chatboxType: .otherUser),
+        Message(content: "How are you doing?", chatboxType: .otherUser),
+        Message(content: "I'm just a computer program, so I don't have feelings, but I'm here and ready to help you with any questions or tasks you have. How can I assist you today?", chatboxType: .currentUserViewService),
+        Message(content: "Tell me a joke.", chatboxType: .otherUser),
+        Message(content: "Certainly! Here's one for you: Why don't scientists trust atoms? Because they make up everything!", chatboxType: .currentUser),
+        Message(content: "How far away is the Moon from the Earth?", chatboxType: .otherUserViewService),
+        Message(content: "The average distance from the Moon to the Earth is about 238,855 miles (384,400 kilometers). This distance can vary slightly because the Moon follows an elliptical orbit around the Earth, but the figure I mentioned is the average distance.", chatboxType: .currentUser)
+        
+    ]
+}
+
+enum ChatBoxType {
+    case currentUser
+    case otherUser
+    case currentUserViewService
+    case otherUserViewService
+}
+
 #Preview {
-    ChatBoxView()
+    ChatBoxView(msg: Message(content: "Hello [User's Name],\nI am interested in your profile.", chatboxType: .currentUserViewService))
 }
--- Kryupa/Core/Chat/ChatView.swift
+++ Kryupa/Core/Chat/ChatView.swift
@@ -8,8 +8,159 @@
 import SwiftUI
 
 struct ChatView: View {
+
+    @Environment(\.router) var router
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:20){
+            HeaderView
+            usernameView
+            requestView
+            
+            ScrollViewReader { proxy in
+                ScrollView {
+                    ForEach(DataSource.messages, id:\.self) {
+                        msg in
+                        
+                        ChatBoxView(msg: msg)
+                    }
+                }
+            }
+            .padding(.horizontal, 22)
+            
+            
+            sendMessageView
+        }
+    }
+    
+    private var sendMessageView: some View{
+        
+        @State var yourText: String = ""
+
+
+        return HStack {
+            Image("camera")
+                .resizable()
+                .frame(width: 39,height: 29)
+                .asButton(.press) {
+                }
+            
+            HStack {
+                TextField("Hello!", text: $yourText)
+                    .padding()
+                    .foregroundStyle(.gray)
+                    .font(.custom(FontContent.plusRegular, size: 17))
+                    .frame(height: 36)
+                    
+
+                Spacer()
+                
+                Image("audio")
+                    .dynamicTypeSize(.medium)
+                    .frame(width: 44,height: 44)
+                    .asButton(.press) {
+                    }
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 18)
+                    .inset(by: 1)
+                    .stroke(.E_5_E_5_EA, lineWidth: 1)
+            )
+
+        }
+        .frame(height: 44)
+        .padding(.horizontal, 20)
+    }
+    
+    private var requestView: some View{
+        
+        VStack(spacing: 10) {
+            Text("Would you like to give service to Alexa?")
+                .font(.custom(FontContent.besRegular, size: 15))
+                .padding(.horizontal, 20)
+
+            HStack(spacing: 10) {
+                Text("Accept")
+                    .font(.custom(FontContent.plusRegular, size: 16))
+                    .foregroundStyle(.white)
+                    .frame(height: 32)
+                    .frame(width: 97)
+                    .background{
+                        RoundedRectangle(cornerRadius: 48)
+                    }
+                    .asButton(.press) {
+                        
+                    }
+                            
+                Text("Decline")
+                    .font(.custom(FontContent.plusRegular, size: 16))
+                    .foregroundStyle(.red)
+                    .frame(height: 32)
+                    .frame(width: 99)
+                    .asButton(.press) {
+                        
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 48)
+                            .inset(by: 1)
+                            .stroke(.red, lineWidth: 1)
+                    )
+            }
+        }
+        .frame(height: 86)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .inset(by: 1)
+                .stroke(.E_5_E_5_EA, lineWidth: 1)
+        )
+    }
+    
+    private var usernameView: some View{
+        
+        HStack(spacing: 20) {
+            
+            Text("Alexa Chatterjee")
+                .font(.custom(FontContent.besRegular, size: 20))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            HStack {
+                Image("phone-call")
+                    .resizable()
+                    .frame(width: 24,height: 24)
+                    .asButton(.press) {
+                    }
+                
+                Image("video")
+                    .resizable()
+                    .frame(width: 24,height: 24)
+                    .asButton(.press) {
+                    }
+            }
+        }
+        .padding(.horizontal, 24)
+        
+        
+    }
+    
+    private var HeaderView: some View{
+        ZStack{
+            Image("KryupaLobby")
+                .resizable()
+                .frame(width: 124,height: 20)
+            
+            HStack{
+                Image("navBack")
+                    .resizable()
+                    .frame(width: 30,height: 30)
+                    .asButton(.press) {
+                        router.dismissScreen()
+                    }
+                Spacer()
+                Image("NotificationBellIcon")
+                    .frame(width: 25,height: 25)
+            }
+            .padding(.horizontal,24)
+        }
     }
 }
 
