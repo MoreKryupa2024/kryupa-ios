9b5ddb75a2403b10e1b51554267e11a14d8eefac
poojaNenavaMoreYeahs <pooja.nenava@moreyeahs.in>
2024-06-10 19:57:40 +0530

    Personal Details

---
+++ Kryupa/Core/Accounts/PersonalDetailViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PersonalDetailViewModel.swift
+//  Kryupa
+//
+//  Created by Pooja Nenava on 10/06/24.
+//
+
+import Foundation
+
+class PersonalDetailViewModel: ObservableObject{
+    
+    @Published var additionalInfoSelected: [String] = [String]()
+
+}
